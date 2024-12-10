cbe1afa5030f73f635a41f92b449e5a0fa246a82
MarkusIsaksson1982 <mjisaksson@gmail.com>
2024-12-10 20:26:39 +0100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,164 +1,90 @@
-/*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+/* I have utilized ChatGPT and Perplexity as resources for guidance and learning throughout this project. My approach reflects the growing trend of modern developers using AI tools to enhance their coding processes. However, all the final code presented here is my own work, based on own independently thought out prompts and without copying prompts or code from others other than snippets. I believe this practice aligns with the principles of academic honesty, as it emphasizes learning and using technology responsibly. */
 
 'use strict';
-const { v4: uuidv4 } = require('uuid');
 
-let books = {}; // In-memory storage for books
+const mongoose = require('mongoose');
+
+// MongoDB connection
+mongoose.connect(process.env.DB, {});
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  comments: { type: [String], default: [] },
+});
+
+const Book = mongoose.model('Book', bookSchema);
 
 module.exports = function (app) {
-  // Route to handle all books
   app.route('/api/books')
-    .get((req, res) => {
-      const allBooks = Object.values(books).map(book => ({
+    .get(async (req, res) => {
+      const books = await Book.find({});
+      res.json(books.map(book => ({
         _id: book._id,
         title: book.title,
         commentcount: book.comments.length,
-      }));
-      res.json(allBooks);
+      })));
     })
-    .post((req, res) => {
+    .post(async (req, res) => {
       const { title } = req.body;
       if (!title) {
-        return res.status(400).send('missing required field title');
+        return res.status(200).send('missing required field title');
       }
-      const newBook = {
-        _id: uuidv4(),
-        title,
-        comments: [],
-      };
-      books[newBook._id] = newBook;
-      res.json(newBook);
+      const newBook = new Book({ title });
+      await newBook.save();
+      res.json({ _id: newBook._id, title: newBook.title });
     })
-    .delete((req, res) => {
-      books = {};
+    .delete(async (req, res) => {
+      await Book.deleteMany({});
       res.send('complete delete successful');
     });
 
-  // Route to handle individual books
   app.route('/api/books/:id')
-    .get((req, res) => {
-      const book = books[req.params.id];
-      if (!book) {
-        return res.status(404).send('no book exists');
+    .get(async (req, res) => {
+      const { id } = req.params;
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(200).send('no book exists');
       }
-      res.json(book);
-    })
-    .post((req, res) => {
-      const book = books[req.params.id];
-      const { comment } = req.body;
+      const book = await Book.findById(id);
       if (!book) {
-        return res.status(404).send('no book exists');
+        return res.status(200).send('no book exists');
+      }
+      res.json({
+        _id: book._id,
+        title: book.title,
+        comments: book.comments,
+      });
+    })
+    .post(async (req, res) => {
+      const { id } = req.params;
+      const { comment } = req.body;
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(200).send('no book exists');
+      }
+      const book = await Book.findById(id);
+      if (!book) {
+        return res.status(200).send('no book exists');
       }
       if (!comment) {
-        return res.status(400).send('missing required field comment');
+        return res.status(200).send('missing required field comment');
       }
       book.comments.push(comment);
-      res.json(book);
+      await book.save();
+      res.json({
+        _id: book._id,
+        title: book.title,
+        comments: book.comments,
+      });
     })
-    .delete((req, res) => {
-      const book = books[req.params.id];
-      if (!book) {
-        return res.status(404).send('no book exists');
+    .delete(async (req, res) => {
+      const { id } = req.params;
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(200).send('no book exists');
       }
-      delete books[req.params.id];
+      const book = await Book.findById(id);
+      if (!book) {
+        return res.status(200).send('no book exists');
+      }
+      await Book.deleteOne({ _id: id });
       res.send('delete successful');
     });
 };
-
-'use strict';
-const { v4: uuidv4 } = require('uuid');
-
-let projects = {}; // In-memory store for simplicity
-
-module.exports = function (app) {
-  // POST request to add a new issue
-  app.route('/api/issues/:project')
-    .post((req, res) => {
-      const { project } = req.params;
-      const { issue_title, issue_text, created_by, assigned_to = '', status_text = '' } = req.body;
-
-      if (!issue_title || !issue_text || !created_by) {
-        return res.status(400).json({ error: 'required field(s) missing' });
-      }
-
-      const newIssue = {
-        _id: uuidv4(),
-        issue_title,
-        issue_text,
-        created_by,
-        assigned_to,
-        status_text,
-        created_on: new Date(),
-        updated_on: new Date(),
-        open: true,
-      };
-
-      if (!projects[project]) projects[project] = [];
-      projects[project].push(newIssue);
-
-      res.json(newIssue);
-    })
-
-    // GET request to fetch issues
-    .get((req, res) => {
-      const { project } = req.params;
-      const filters = req.query;
-
-      if (!projects[project]) return res.json([]);
-
-      let filteredIssues = projects[project];
-
-      // Apply filters
-      for (let key in filters) {
-        filteredIssues = filteredIssues.filter(issue => issue[key] == filters[key]);
-      }
-
-      res.json(filteredIssues);
-    });
-
-  app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-    })
-    
-    .post(function (req, res){
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
-    })
-    
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
-    });
-
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
-      let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
-    })
-    
-    .post(function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
-      //json res format same as .get
-    })
-    
-    .delete(function(req, res){
-      let bookid = req.params.id;
-      //if successful response will be 'delete successful'
-    });
-  
-
-  // PUT and DELETE endpoints will be implemented next...
-};
-
-
-
